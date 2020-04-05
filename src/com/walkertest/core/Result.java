fb2e406effe5daae175493c15050b5cae848ea0c
habit <habit@DESKTOP-QNF5QFB>
2020-04-06 00:01:39 +0100

    Finished documenting a few classes

--- src/com/walkertest/core/Result.java
+++ src/com/walkertest/core/Result.java
@@ -3,26 +3,60 @@ package com.walkertest.core;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The {@code Result} class represents the result for a test.
+ * The scores obtained for each {@link Part} are given with
+ * the {@link getScoresByPart()} method.
+ * 
+ *
+ * @param <K>
+ * the type of object to use as the {@code Question}s' ids
+ * @param <V>
+ * the type of object to use as the {@code Question}s' raw data
+ */
 public class Result <K, V>{
 	private User<?, ?> user;
 	private Map<Part<K, V>, Integer> scoresByPart = new HashMap<>();
 	
+	/**
+	 * Gets the user that owns this {@code Result}
+	 * @return
+	 * the {@link User} that owns this result
+	 */
 	public User<?, ?> getUser() {
 		return user;
 	}
 	
+	/**
+	 * Sets the user that owns this {@code Result}
+	 * @param user
+	 * the owner of this {@code Result}
+	 */
 	public void setUser(User<?, ?> user) {
 		this.user = user;
 	}
 
+	/**
+	 * Gets the scores of each {@code Part} taken during the test
+	 * @return
+	 * a map containing the scores for each part of a test
+	 */
 	public Map<Part<K, V>, Integer> getScoresByPart() {
 		return scoresByPart;
 	}
 
+	/**
+	 * Sets the scores of the parts of the marked test
+	 * @param scoresByPart
+	 * a map of the scores of each part of the test
+	 */
 	public void setScoresByPart(Map<Part<K, V>, Integer> scoresByPart) {
 		this.scoresByPart = scoresByPart;
 	}
 	
+	/*
+	 * This method was overridden due to irregularities during unit tests
+	 */
 	@Override
 	public boolean equals(Object other) {
 		if(other instanceof Result) {
--- src/com/walkertest/core/TestMarker.java
+++ src/com/walkertest/core/TestMarker.java
@@ -3,14 +3,47 @@ package com.walkertest.core;
 import java.util.*;
 import com.walkertest.question.*;
 
-
+/**
+ * The {@code TestMarker} class contains methods for marking {@code Test}s
+ * 
+ *
+ */
 public final class TestMarker {
 	
+	/**
+	 * Marks a given {@code Test} instance and gives the {@code Result}.
+	 * 
+	 * @param <K>
+	 * the type of object to use as the {@code Question}s' id
+	 * @param <V>
+	 * the type of object to use as the {@code Question}s' raw data
+	 * @param test
+	 * the {@code Test} instance to be marked
+	 * @return
+	 * a {@code Result} containing the scores obtained for this {@code Test}
+	 */
 	public static <K, V> Result<K, V> scoreTest(Test<K, V> test) {
 		Map<Part<K, V>, Integer> scoresByPart = markByParts(test.getParts());
 		return putIntoResult(test.getUser(), scoresByPart);
 	}
 	
+	/**
+	 * Marks the given Set of {@code Part}s. 
+	 * <p>
+	 * The result of the marking is given as a Map which
+	 * uses {@code Part} as its key and an integer as its value.
+	 * The integer value is the score obtained for that Part
+	 * 
+	 * @param <K>
+	 * the type of object to use as the {@code Question}s' id
+	 * @param <V>
+	 * the type of object to use as the {@code Question}s' raw data
+	 * @param parts
+	 * the set of {@code Part}s to be marked
+	 * @return
+	 * a map in which the integer values represent the scores obtained
+	 * for the respective {@code Part}
+	 */
 	public static <K, V> Map<Part<K, V>, Integer> markByParts(Set<Part<K, V>> parts){
 		Map<Part<K, V>, Integer> scoresByPart = new HashMap<>();
 		
@@ -23,6 +56,9 @@ public final class TestMarker {
 	}
 	
 
+	/*
+	 * Marks all the Questions for the given part
+	 */
 	private static <K, V> Integer markQuestionsForPart(Part<K, V> part) {
 		Set<Answer<K, V>> answers = part.getAnswers();
 		int score = 0;
